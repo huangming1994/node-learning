f7cc0dbce039bfb7a496edf5667cfd653b65daa8
huangming1994 <1157114498@qq.com>
2017-08-22 13:44:19 +0800

    use async instead eventproxy

--- src/lesson4/eventproxy.js
+++ src/lesson4/eventproxy.js
@@ -3,10 +3,11 @@ const eventproxy = require('eventproxy')
 const cheerio = require('cheerio')
 const superagent = require('superagent')
 const url = require('url')
+const async = require('async')
 
 const app = express()
 const cnodeUrl = 'http://cnodejs.org/'
-
+// eventproxy控制并发
 superagent.get(cnodeUrl)
 .then(function(res){
   const topicUrls = []
@@ -47,4 +48,27 @@ superagent.get(cnodeUrl)
 
 app.listen('3456', function(){
   console.log('ok')
+})
+
+
+// async控制并发
+const topicUrls = []
+superagent.get(cnodeUrl)
+.then(function(res) {
+  const $ = cheerio.load(res.text)
+  $('#topic_list .topic_title')
+  .each(function (index, ele) {
+    const $element = $(ele)
+    const href = url.resolve(cnodeUrl, $element.attr('href'))
+    topicUrls.push(href)
+  })
+  async.mapLimit(topicUrls, 3, (url, callback) => {
+    superagent.get(url)
+    .end(function(err, res){
+      if(err) console.log('err', err)
+      console.log(`${url}请求成功`)
+      callback(null, res)
+    })
+  }, (err, result) => {
+  })
 })
--- src/lesson5/async.js
+++ src/lesson5/async.js
@@ -4,7 +4,7 @@ let concurrencyCount = 0
 const fetchAPI = (url, callback) => {
   const delay = parseInt(Math.random() * 2000)
   concurrencyCount++
-  console.log(`现在的并发数是,${concurrencyCount},正在抓取的是,${url},耗时${delay}毫秒`);
+  console.log(`现在的并发数是${concurrencyCount},正在抓取的是${url},耗时${delay}毫秒`);
   setTimeout(() => {
     concurrencyCount--
     callback()
@@ -12,7 +12,6 @@ const fetchAPI = (url, callback) => {
 }
 
 const urls = Array.from({ length: 30 }, (v, i) => `http://datasource_${i}`)
-
 async.mapLimit(urls, 5, function (url, callback) {
   fetchAPI(url, callback);
 }, function (err, result) {
